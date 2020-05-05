e12b153beba09337b314277609e3a331e705f8bb
CrackTheCode016 <ibnbassem@gmail.com>
2020-05-05 12:02:18 -0400

    added openssl

--- src/install_base_deps_ubuntu.sh
+++ src/install_base_deps_ubuntu.sh
@@ -23,7 +23,7 @@ function install_base_deps {
     base_deps=(autoconf libtool cmake curl git xz-utils
         libatomic-ops-dev libunwind-dev g++ gdb libgflags-dev
         libsnappy-dev ninja-build python3 python3-ply build-essential
-        python-dev libxml2-dev libxslt-dev libssl-dev)
+    python-dev libxml2-dev libxslt-dev libssl-dev)
     install_apt_dependency
 }
 
@@ -38,12 +38,12 @@ function install_gcc {
     echo
     
     # courtesy of: https://linuxize.com/post/how-to-install-gcc-compiler-on-ubuntu-18-04/
-
+    
     sudo apt install software-properties-common
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-
+    
     sudo apt install gcc-${gcc_version} g++-${gcc_version}
-
+    
     # update alternative - warning, it will this version of gcc as the prioritized version on your system
     
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${gcc_version} 90
@@ -67,6 +67,34 @@ function install_cmake {
     make
     make install
 }
+
+
+
+# region openssl
+
+function install_openssl {
+    openssl_version=1.1.1f
+    echo "Installing OpenSSL ${openssl_version}"
+    echo
+    mkdir /opt/openssl
+    tar xfvz ~/Downloads/openssl-${openssl_version}.tar.gz --directory /opt/openssl
+    cd /opt/openssl/openssl-1.1.1b
+    ./config --prefix=/opt/openssl --openssldir=/opt/openssl/ssl
+    make
+    make install
+    updatedb
+    cd /usr/bin
+    # rename old version
+    mv openssl openssl.old
+    touch /etc/profile.d/openssl.sh
+    printf "#!/bin/sh \n
+    export PATH=/opt/openssl/bin:${PATH} \n
+    export LD_LIBRARY_PATH=/opt/openssl/lib:${LD_LIBRARY_PATH}" > /etc/profile.d/openssl.sh
+    chmod +x /etc/profile.d/openssl.sh
+    source /etc/profile.d/openssl.sh
+}
+
+# endregion
 
 # endregion
 
